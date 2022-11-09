fe16eeb230a89ae28e5ed8204da4900faa679024
James Welchman <jamesw@plantpot.ai>
2022-11-09 12:59:07 +0000

    improved heading and banners

--- nav.js
+++ nav.js
@@ -1,18 +1,23 @@
 const navButton = document.getElementById('menu-button')
 const navBar = document.getElementById('navbar')
-const navIcon = document.getElementById('menu-icon')
+const navIcon = document.getElementById('control-bar__navicon')
 let navBarOpen = false
 
-navButton.addEventListener('click', _ => {
-    if (!navBarOpen) {
-	navBar.classList.remove('nav-is-open')
-	navBar.classList.add('nav-is-open')
-	navIcon.src = "../images/47029440df7f75de6acc77333c4d54067a1f0870d82ade5ef0092edce7246659.png"
-	navBarOpen = true
-    } else {
-	navBar.classList.remove('nav-is-open')
-	navBar.classList.add('nav-is-closed')
-	navIcon.src = "../images/04e86f62a3a279f7dc7d4feb4bc9d99d739692029b71bd8293bcc86e848d6914.png"
-	navBarOpen = false
-    }
-});
+// Footer
+const footer = document.getElementById('footer')
+
+navButton.onclick = () => {
+  if (!navBarOpen) {
+    navBar.classList.remove('nav-is-closed')
+    navBar.classList.add('nav-is-open')
+    navIcon.src =
+        "../images/47029440df7f75de6acc77333c4d54067a1f0870d82ade5ef0092edce7246659.png"
+    navBarOpen = true
+  } else {
+    navBar.classList.remove('nav-is-open')
+    navBar.classList.add('nav-is-closed')
+    navIcon.src =
+        "../images/04e86f62a3a279f7dc7d4feb4bc9d99d739692029b71bd8293bcc86e848d6914.png"
+    navBarOpen = false
+  }
+}
--- site.py
+++ site.py
@@ -31,7 +31,8 @@ RUS = Path("./rus")
 
 
 def iterate_pics(data_dict):
-    for fname in chain(GALLERY.glob('*'), GAMES.glob('*'), CHARACTERS.glob('*'), FLAGS.glob('*')):
+    for fname in chain(GALLERY.glob('*'), GAMES.glob('*'),
+                       CHARACTERS.glob('*'), FLAGS.glob('*')):
         if str(fname.parent) not in data_dict:
             data_dict[str(fname.parent)] = {}
 
@@ -41,7 +42,7 @@ def iterate_pics(data_dict):
         name = hash + '.' + ext
         data_dict[str(fname.parent)][fname.name] = "../images/" + name
         yield name, data
-            
+
 
 def make_images(data):
     if not IMAGES.is_dir():
@@ -54,17 +55,6 @@ def make_images(data):
 
     with open("images.json", 'w') as file:
         js.dump(images, file)
-
-
-def make_css():
-    with open("style.css", 'rb') as file:
-        css = file.read()
-        digest = sha256(css).digest().hex()
-
-    name = "style_" + digest[:12] + '.css'
-
-    open(name, 'wb').write(css)
-    return name
 
 
 def do_english(data):
@@ -135,9 +125,6 @@ def main():
     data['images'] = {}
     make_images(data['images'])
 
-    # CSS
-    data['css'] = make_css()
-
     # Images
     for char in data['strings']['characters']:
         char['image'] = data['images']['characters'][char['image']]
@@ -146,19 +133,29 @@ def main():
         mkdir(EN)
 
     do_english(data)
-    for page in (INDEX, FAQ, PRICES, CHARS_AND_GAMES, PICS_AND_TESTI, CONTACT, EVENT):
+    for page in (INDEX, FAQ, PRICES, CHARS_AND_GAMES, PICS_AND_TESTI, CONTACT,
+                 EVENT):
         content = Template(filename=str(page)).render(**data)
         with open(EN / page.name, "w") as file:
-            file.write(HEADER.render(content=content, prefix="/en", current_page=page.name, **data))
+            file.write(
+                HEADER.render(content=content,
+                              prefix="/en",
+                              current_page=page.name,
+                              **data))
 
     if not RUS.is_dir():
         mkdir(RUS)
 
     do_russian(data)
-    for page in (INDEX, FAQ, PRICES, CHARS_AND_GAMES, PICS_AND_TESTI, CONTACT, EVENT):
+    for page in (INDEX, FAQ, PRICES, CHARS_AND_GAMES, PICS_AND_TESTI, CONTACT,
+                 EVENT):
         content = Template(filename=str(page)).render(**data)
         with open(RUS / page.name, "w") as file:
-            file.write(HEADER.render(content=content, prefix="/rus", current_page=page.name, **data))
+            file.write(
+                HEADER.render(content=content,
+                              prefix="/rus",
+                              current_page=page.name,
+                              **data))
 
 
 if __name__ == '__main__':
